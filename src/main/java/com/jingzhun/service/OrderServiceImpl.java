cf22ee321fa0a958b45e6e59cc759605eb885c15
wdwhwn <wdwhwn@163.com>
2019-03-06 09:46:37 +0800

    dishwasher Second upload

---
+++ src/main/java/com/jingzhun/service/OrderServiceImpl.java
@@ -0,0 +1,77 @@
+package com.jingzhun.service;
+import com.jingzhun.dao.AddressDao;
+import com.jingzhun.dao.GoodsDao;
+import com.jingzhun.dao.OrderDao;
+import com.jingzhun.dao.UserDao;
+import com.jingzhun.entity.*;
+import com.jingzhun.utils.properties.PropertiesUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+
+/**
+ * Created by Administrator on 2019/3/5 0005.
+ */
+@Service
+@Transactional
+public class OrderServiceImpl implements  OrderService {
+    @Autowired
+    private GoodsDao goodsDao;
+    @Autowired
+    private AddressDao addressDao;
+    @Autowired
+    private OrderDao orderDao;
+    @Autowired
+    private UserDao userDao;
+//    确认商品订单
+    @Override
+    public Goods selectByGoodsId(Integer goodsId) throws FileNotFoundException {
+        Goods goods = goodsDao.selectByGoodsId(goodsId);
+        String urlName = PropertiesUtil.readValue("main.properties", "urlName");
+        String url = PropertiesUtil.readValue("url.properties", urlName);
+        GoodsImg goodsImg = goods.getGoodsImgList().get(0);
+        String imgUrl=url+goodsImg.getGoodsImgImg();
+        goodsImg.setGoodsImgImg(imgUrl);
+        goods.getGoodsImgList().set(0,goodsImg);
+        System.out.println("123456");
+        return goods;
+    }
+    //  确认收货人信息1
+    @Override
+    public Address addressCheck1(Integer userId) {
+        Address address = addressDao.selectByDefault(userId);
+        return address;
+    }
+    //  确认收货人信息2
+    @Override
+    public Address addressCheck2(Integer userId,Integer addressId) {
+        Address address = addressDao.selectByAddressId(userId,addressId);
+        return address;
+    }
+
+    @Override
+    public String insertOrder(Order order) {
+//        判断积分是否充足
+        Integer orderUserId = order.getOrderUserId();
+        User user = userDao.selectByPrimaryKey(orderUserId);
+        BigDecimal userCurrentScore = user.getUserCurrentScore();
+        int i = userCurrentScore.compareTo(order.getOrderTotalPrice());
+        if(i<0){
+            return "积分余额不足";
+        }
+        orderDao.insertOrder(order);
+        BigDecimal orderTotalPrice = order.getOrderTotalPrice();
+        BigDecimal subtract = userCurrentScore.subtract(orderTotalPrice);
+        BigDecimal userConsumeScore = user.getUserConsumeScore();
+        BigDecimal add = userConsumeScore.add(orderTotalPrice);
+        user.setUserCurrentScore(subtract);
+        user.setUserConsumeScore(add);
+        userDao.insert(user);
+        return "订单提交成功";
+    }
+
+
+}
